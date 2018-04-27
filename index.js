73b8fa3a62bad49910f9ccb11ed7c96a20a189ff
Chase Lau <chase9@mac.com>
2018-04-26 22:07:12 -0500

    Working searching

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let sqlite = require('sqlite3').verbose();
+let poster = require('./imdb_poster');
 
 let port = 8009;
 let app = express();
@@ -33,36 +34,69 @@ app.post('/search', urlParser, (req, res) => {
 
     switch (type) {
         case 'actor':
-            sql = "SELECT * FROM Names WHERE primary_name LIKE ?";
-            params.push(search);
+            sql = "SELECT * FROM Names WHERE primary_name LIKE ? LIMIT 100";
+            params.push('%' + search + '%');
             break;
         default:
-            sql = "SELECT * FROM Titles WHERE primary_title LIKE ? AND title_type is ?";
-            params.push(search);
+            sql = "SELECT * FROM Titles WHERE primary_title LIKE ? AND title_type is ? LIMIT 100";
+            params.push('%' + search + '%');
             params.push(type);
+            break;
     }
 
     db.all(sql, params, (err, rows) => {
         if (err) return err;
 
-        if (rows.length === 0) {
-            res.render('results', {searchTitle: search, results: "nothing!"})
+        if (rows.length === 0 || rows === null) {
+            res.render('results', {searchTitle: search, results: [{'name': "No results found!"}]});
+            res.end();
+            return;
         }
-
         let data = [];
         rows.forEach((row) => {
-            let item = {
-                'name': row.primary_title || row.primary_name
-            };
-            data.push(item);
+            /** @namespace row.primary_name */
+            /** @namespace row.primary_profession */
+            /** @namespace row.primary_title */
+            /** @namespace row.genres */
+            /** @namespace row.nconst */
+            /** @namespace row.tconst */
+
+            // Search was for actors, else media
+            if (row.primary_name !== undefined) {
+                let item = {
+                    'name': row.primary_name,
+                    'subtitle': row.birth_year + " - " + (row.death_year || "present"),
+                    'description': row.primary_profession.replace(',', ', '),
+                    'link': '/people/' + row.nconst,
+                };
+                data.push(item);
+            } else {
+                let item = {
+                    'name': row.primary_title,
+                    'subtitle': row.start_year + " - " + (row.end_year || "present"),
+                    'description': (row.genres || "").replace(',', ', '),
+                    'link': '/media/' + row.tconst,
+                };
+                data.push(item);
+            }
         });
-        res.render('results', {searchTitle: search, results: data})
+        res.render('results', {searchTitle: search, results: data});
+        res.end();
     })
+});
+
+app.get('/people/*', (req, res) => {
+
+});
+
+app.get('/titles/*', (req, res) => {
+
 });
 
 // Send all other requests to homepage
 app.get('*', (req, res) => {
     res.redirect('/');
+    res.end();
 });
 
 app.listen(port);
